4cd4a5bd22f8016de0a8dc124fb9b878c52e1303
Ligia Dolinta <ligiadolinta@gmail.com>
2019-08-11 11:22:01 +0300

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,32 @@ import { connect } from 'react-redux';
 import history from '../history';
 
 
-import { Slide } from 'react-slideshow-image';
+// import { Slide } from 'react-slideshow-image';
  
-const slideImages = [
-    'photos/snow.jpg',
-   'photos/slide2.jpg',
-    'photos/slide3.jpg',
-    'photos/slide4.jpg',
-    'photos/slide5.jpg',
-    'photos/slide6.jpg',
-    'photos/slide7.jpg',
-    'photos/slide8.jpg',
-    'photos/slide9.jpg',
-    'photos/slide10.jpg',
+// const slideImages = [
+//     'photos/snow.jpg',
+//    'photos/slide2.jpg',
+//     'photos/slide3.jpg',
+//     'photos/slide4.jpg',
+//     'photos/slide5.jpg',
+//     'photos/slide6.jpg',
+//     'photos/slide7.jpg',
+//     'photos/slide8.jpg',
+//     'photos/slide9.jpg',
+//     'photos/slide10.jpg',
     
-];
+// ];
  
-const properties = {
-  duration: 6000,
-  transitionDuration: 350,
-  infinite: true,
-  indicators: false,
-  arrows: true,
-  onChange: (oldIndex, newIndex) => {
-    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-  }
-}
+// const properties = {
+//   duration: 6000,
+//   transitionDuration: 350,
+//   infinite: true,
+//   indicators: false,
+//   arrows: true,
+//   onChange: (oldIndex, newIndex) => {
+//     console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+//   }
+// }
 
 
    
@@ -38,157 +38,158 @@ class Header extends Component {
     render() {
 
         return (
+            <div> <h1> Whatever</h1></div>
 
-         <Slide {...properties} className = 'container'>
+        //  <Slide {...properties} className = 'container'>
            
-            {/* <img classname='header__img' src={slideImages[0]} />
-            <img classname='header__img' src={slideImages[1]} />
-            <img classname='header__img' src={slideImages[2]} />
-            <img classname='header__img' src={slideImages[3]} /> */}
+        //     {/* <img classname='header__img' src={slideImages[0]} />
+        //     <img classname='header__img' src={slideImages[1]} />
+        //     <img classname='header__img' src={slideImages[2]} />
+        //     <img classname='header__img' src={slideImages[3]} /> */}
        
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                  <h1 className='header__text'>Let's Go For A Ride!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[0]})`}}>
+        //           <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
         
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                  <h1 className='header__text'>Enjoy The Best Adventure With Us!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[1]})`}}>
+        //           <h1 className='header__text'>Enjoy The Best Adventure With Us!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
-            <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                 <h1 className='header__text'>Best Prices, Best Quality </h1>
-                <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
+        //     <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[2]})`}}>
+        //          <h1 className='header__text'>Best Prices, Best Quality </h1>
+        //         <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
 
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[3]})`}}>
-                  <h1 className='header__text'>Let's Go For A Ride!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[3]})`}}>
+        //           <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
-            <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[4]})`}}>
-                 <h1 className='header__text'>Let's Go For A Ride!</h1>
-                <div className='header__links'>
-                   {
-                        this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
+        //     <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[4]})`}}>
+        //          <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //         <div className='header__links'>
+        //            {
+        //                 this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
 
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[5]})`}}>
-                  <h1 className='header__text'>Let's Go For A Ride!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[5]})`}}>
+        //           <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
 
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[6]})`}}>
-                  <h1 className='header__text'>Let's Go For A Ride!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[6]})`}}>
+        //           <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
 
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[7]})`}}>
-                  <h1 className='header__text'>Let's Go For A Ride!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[7]})`}}>
+        //           <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
 
-             <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[8]})`}}>
-                  <h1 className='header__text'>Let's Go For A Ride!</h1>
-                 <div className='header__links'>
-                    {
-                         this.props.headerLinks.map((link, index) => {
-                              return (
-                                  <a key={index} className='header__link' onClick={() => history.push(link.path)}>
-                                      {link.title}
-                                  </a>
+        //      <div className = 'header__img' style={{'backgroundImage': `url(${slideImages[8]})`}}>
+        //           <h1 className='header__text'>Let's Go For A Ride!</h1>
+        //          <div className='header__links'>
+        //             {
+        //                  this.props.headerLinks.map((link, index) => {
+        //                       return (
+        //                           <a key={index} className='header__link' onClick={() => history.push(link.path)}>
+        //                               {link.title}
+        //                           </a>
 
-                              )
-                          })
-                      }
-                  </div>
-             </div>
+        //                       )
+        //                   })
+        //               }
+        //           </div>
+        //      </div>
 
     
-        </Slide>
+        // </Slide>
     
         )
     }
